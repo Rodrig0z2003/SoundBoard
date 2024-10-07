830fbe89f8400cfa7be2f41af82a57d3405b9fdc
Rodrig0z2003 <sandro.capira@tecsup.edu.pe>
2024-10-07 09:00:33 -0500

    Primer commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Sandro Rodrigo on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
